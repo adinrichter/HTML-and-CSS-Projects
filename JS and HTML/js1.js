422e527eb7fe0c75a008a5096990866aac042cda
adinrichter <adinrichter@gmail.com>
2022-04-29 13:38:48 -0700

    Actually added the files I meant to

---
+++ JS and HTML/js1.js
@@ -0,0 +1,2 @@
+alert("This website is currently under development. 
+\nAs such, any and all parts may be entirely non-functional.")
---
+++ JS and HTML/js2.js
@@ -0,0 +1,4 @@
+function clicked() {
+  var string = "Clack.";
+  document.getElementById("Button").innerHTML = string;
+}
